c7cf9284501bcfa6f657915245403b4e858811ed
siaoynli <siaoynli@126.com>
2020-07-21 14:26:03 +0800

    添加env-cmd 配置文件 .env-cmdrc.json

--- config-overrides.js
+++ config-overrides.js
@@ -4,7 +4,7 @@
  * @Github: http://github.com/siaoynli
  * @Date: 2020-07-20 15:56:52
  * @LastEditors: lixiaoyun
- * @LastEditTime: 2020-07-20 16:18:29
+ * @LastEditTime: 2020-07-20 17:14:53
  * @Description: 按需加载
  * create-react-app 官方推荐的一个库 customize-cra 扩展文件,
  * 安装  react-app-rewired babel-plugin-import less  less-loader@5   env-cmd  -D
@@ -20,7 +20,13 @@ module.exports = override(
     libraryDirectory: 'es',
     style: true, //true表示引入less
   }),
+  //这里设置less，定制antd 主题
+  //https://ant.design/docs/react/customize-theme-cn
   addLessLoader({
     javascriptEnabled: true,
+    modifyVars: {
+      '@primary-color': '#d214a2',
+      '@font-size-base': '12px',
+    },
   })
 )
